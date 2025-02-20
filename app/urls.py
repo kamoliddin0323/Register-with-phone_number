a1e4c76ade2a2e81a684c45edb5a768fbf0534d6
Пользователь <qutbiddinovkamoliddin797@gmail.com>
2025-02-20 14:50:41 +0500

    last

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('register/', views.UserRegisterAPIView.as_view()),
@@ -8,3 +12,7 @@ urlpatterns = [
     path("token-test/", views.TestTokenAPIView.as_view()),
     path("logout/", views.LogOutAPIView.as_view()),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
